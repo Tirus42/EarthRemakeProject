b5e07204fbf2d9800a792918123e0b2e8eae6150
Tirus <Tirus@92bb1c38-9b61-c64a-b0c3-bf70b591afdc>
2013-09-20 22:50:11 +0000

    Teil des Grundgerüstes für Gebäude erstellt
    Klasse MapBuildings:
    - Hauptverwaltungsklasse für alle Gebäude auf der Karte.
    - Bietet Schnittstelle für Observer Klassen.
    - Für eine optimalere Verwaltung werden die Daten in Position und
      eigendliches Gebäude getrennt.
    
    Klasse BuildingReference:
    - Speichert die Grundfläche (mit Position) des Gebäudes sowie
      eine Referenz, wo weitere Daten über das Gebäude zu finden sind.
    
    Interface IMapBuildingObserver:
    - Interface für Observer der MapBuildings.
    - Löst Events aus, wenn Gebäude erstellt oder entfernt werden.
    
    git-svn-id: https://felix1991.mine.nu/svn/Earth2150@143 92bb1c38-9b61-c64a-b0c3-bf70b591afdc

---
+++ cpp/earth2150/include/Map/MapBuildings.h
@@ -0,0 +1,45 @@
+#ifndef MAPBUILDINGS_H_INCLUDED
+#define MAPBUILDINGS_H_INCLUDED
+
+#include <stdint.h>
+#include <vector>
+
+#include "Util/MemoryBlock.h"
+
+#include "Building/BuildingReference.h"
+
+class IMapBuildingsObserver;
+
+/**
+* Verwaltet alle Gebäude auf einer Map.
+* Bietet eine Schnittstelle für Observer, welche über neue und entfernte Gebäude
+* informiert werden.
+*/
+class MapBuildings {
+	private:
+		/// Referenzliste der Gebäude
+		MemoryBlock<BuildingReference> buildings;
+
+		/// Liste der Observer, welche über Änderungen Benachrichtigt werden sollen
+		std::vector<IMapBuildingsObserver*> observers;
+
+		MapBuildings(const MapBuildings&);
+		MapBuildings operator=(const MapBuildings&);
+
+	public:
+		MapBuildings();
+		~MapBuildings();
+
+		/// Gibt die Anzahl an Gebäuden zurück
+		uint32_t getCount() const;
+
+		/// Fügt einen Observer in die Benachrichtigungsliste ein
+		void attachObserver(IMapBuildingsObserver* observer);
+
+		/// Entfernt einen Observer aus der Benachrichtigungsliste
+		void detachObserver(IMapBuildingsObserver* observer);
+
+};
+
+
+#endif // MAPBUILDINGS_H_INCLUDED
