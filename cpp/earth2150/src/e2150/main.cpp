cc7a02df64248cbc9845e9b6436e26a97e0a9ee7
Tirus <Tirus@92bb1c38-9b61-c64a-b0c3-bf70b591afdc>
2014-05-10 10:54:53 +0000

    Einige Testwegsuchen mit der Klasse PathFinderTester geschrieben: - Dabei diverse Wegpunkte angegeben die verschiedene Szenarien für die   Suchalgorithmen darstellen, u.a.:   - Kurze einfache Strecke   - Einmal Quer über 1024x1024 Karte   - Horizontal über 1024x1024 Karte.   - Einige Karten aus dem Earth Spiel   - Wege mit großen Umwegen   - Punkte die kein Weg verbindet - Alle Tests laufen auf der Aktuellen Testkarte ab. - Zum ausführen der Tests muss das Build Target "Server" mit dem Parameter "-b"   aufgerufen werden.
    git-svn-id: https://felix1991.mine.nu/svn/Earth2150@172 92bb1c38-9b61-c64a-b0c3-bf70b591afdc

--- cpp/earth2150/src/e2150/main.cpp
+++ cpp/earth2150/src/e2150/main.cpp
@@ -6,22 +6,66 @@
 #include "tf/network.h"
 #include <iostream>
 #include <cstdlib>
+#include <unistd.h>	// für getopt()
 
+#include "Tests/PathFinderTester.h"
+#include "PathFinder/NavigatorFactory.h"
+
+void benchmark(const Map& map) {
+	PathFinderTester pt(map);
+
+	pt.registerPathFinderNavigator(NavigatorFactory::getNavigator(map, NavigatorFactory::NAVIGATOR_ASTAR), "AStar");
+	pt.registerPathFinderNavigator(NavigatorFactory::getNavigator(map, NavigatorFactory::NAVIGATOR_BSEARCH), "Breitensuche");
+	pt.registerPathFinderNavigator(NavigatorFactory::getNavigator(map, NavigatorFactory::NAVIGATOR_JPS), "Jump Point Search");
+
+	pt.addSearchPoints(MapPosition(10, 10), MapPosition(20, 10), "Kurze Suche ohne Hindernis");
+	pt.addSearchPoints(MapPosition(20, 500), MapPosition(1015, 500), "Horizontal ueber ganze Karte (ohne Hindernis)");
+	pt.addSearchPoints(MapPosition(1, 1), MapPosition(1021, 1021), "Diagonal ueber ganze Karte");
+	pt.addSearchPoints(MapPosition(148, 20), MapPosition(302, 238), "GroundHill Diagonal");
+	pt.addSearchPoints(MapPosition(225, 24), MapPosition(225, 234), "GroundHill Vertikal");
+	pt.addSearchPoints(MapPosition(10, 20), MapPosition(25, 56), "Berg in Taal (links oben)");
+	pt.addSearchPoints(MapPosition(772, 115), MapPosition(897, 127), "Spiralberg (rechts oben) von unten nach oben");
+	pt.addSearchPoints(MapPosition(790, 227), MapPosition(963, 430), "Durch Labyrinth (rechts oben)");
+	pt.addSearchPoints(MapPosition(774, 433), MapPosition(718, 371), "Durch Spirale ins innere (rechts oben)");
+	pt.addSearchPoints(MapPosition(719, 371), MapPosition(903, 141), "Aus Spirale auf Spiralberg");
+	pt.addSearchPoints(MapPosition(903, 141), MapPosition(719, 371), "Von Spiralberg in Spirale");
+	pt.addSearchPoints(MapPosition(300, 957), MapPosition(399, 983), "Karte \"Teufelsschucht\" einmal durch (links unten)");
+	pt.addSearchPoints(MapPosition(22, 957), MapPosition(23, 954), "Felder nahe aber grosser Umweg (small Worst Case)");
+	pt.addSearchPoints(MapPosition(26, 20), MapPosition(22, 20), "Suche ohne mgl. Weg (beschraenkt)");
+	pt.addSearchPoints(MapPosition(22, 20), MapPosition(26, 20), "Suche ohne mgl. Weg (viel Fl.) (Worst Case!)");
+
+	pt.runTests(10);
+}
 int main(int argc, char *argv[]) {
-	Map m(1024, 1024);
+	bool runBenchmark = false;
+	{
+		int c;
+
+		while ((c = getopt(argc, argv, "b")) != -1) {
+			if (c == 'b')
+				runBenchmark = true;
+		}
+	}
+
+	Map m(1024, 1024);
 	bool result = m.loadHeightMapRAW("map1024x1024.bin");
 	std::cout << "Laden der Map " << (result ? "erfolgreich" : "fehlgeschlagen") << std::endl;
 
 	if (!result)
-		return EXIT_FAILURE;
+		return EXIT_FAILURE;
+
 	// Komplette Wegkarte berechnen
 	m.updateMovementMap();
+
+	if (runBenchmark) {
+		benchmark(m);
+		return 0;
+	}
 
 	m.addSpawnPoint(MapPosition(5, 5));
 	m.addSpawnPoint(MapPosition(20, 10));
 	m.addSpawnPoint(MapPosition(5, 20));
 	m.addSpawnPoint(MapPosition(25, 55));	//Innerhalb der Schlucht
-
 
 	InitNetwork();
 
