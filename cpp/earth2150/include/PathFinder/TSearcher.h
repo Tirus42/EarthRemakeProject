85ddb228d94c8ac4465f686293d6d77a9b9b8b3c
Tirus <Tirus@92bb1c38-9b61-c64a-b0c3-bf70b591afdc>
2014-05-23 22:27:34 +0000

    Breitensuchenimplementierung "TSearcher" in Header und Source getrennt. - Interesannterweise bringt dies laut mehrmaligem Benchmark einen   Speedup von etwa 2-3%.
    git-svn-id: https://felix1991.mine.nu/svn/Earth2150@182 92bb1c38-9b61-c64a-b0c3-bf70b591afdc

--- cpp/earth2150/include/PathFinder/TSearcher.h
+++ cpp/earth2150/include/PathFinder/TSearcher.h
@@ -4,19 +4,6 @@
 #include "Map/Map.h"
 
 #include <stdint.h>
-#include <memory.h>
-
-#define container std::vector
-
-/// Hilfsstruktur für abgesuche Wegpunkte
-struct Waypoint {
-	Waypoint(uint32_t previus, uint32_t position) :
-		searched(false), previus(previus), position(position) {}
-
-	bool searched;
-	uint32_t previus;
-	uint32_t position;
-};
 
 /**
 * Breitensuchen Suchalgorithmus
@@ -29,189 +16,24 @@ class TSearcher {
 
 		static const uint32_t START_WAYPOINT = 0xFFFFFFFF;
 
-		size_t mapSize;	// Rohdatengröße
-		uint8_t* searchMap;	// Such-Bitmap
+		/// Speichert die Rohdatengröße
+		const size_t mapSize;
+
+		/// Bitmap für die Suche
+		uint8_t* searchMap;
 
 		const Map& map;
 
 		/// Suchkarte mit 0-en überschreiben
-		inline void clearSearchMap() {
-			memset(searchMap, 0, mapSize);
-		}
+		void clearSearchMap();
 
 	public:
-		TSearcher(const Map& map) :
-			mapSize(map.getWidth() * map.getHeight()),
-			searchMap(new uint8_t[mapSize]),
-			map(map) {};
+		TSearcher(const Map& map);
 
-		~TSearcher() {
-			delete searchMap;
-		};
+		~TSearcher();
 
 		/// Such-funktion
-		bool FindWay(uint32_t position1, uint32_t position2, std::list<uint32_t>& path_list) {
-			// Zu Begin die Suchkarte leeren
-			clearSearchMap();
-
-			// Punktliste
-			container<Waypoint> waypoints;
-
-			// Genügend Speicher Reservieren, damit es nicht zu unnötigen reallocs kommt
-			waypoints.reserve(map.getWidth() * map.getHeight());
-
-			// Startpunkt in Suchliste eintragen
-			waypoints.push_back(Waypoint(START_WAYPOINT, position1));
-
-			uint32_t position = 0;
-
-			const uint16_t mapWidth = map.getWidth();
-
-			while (true) {
-				for (int32_t i = waypoints.size() - 1; i >= 0; --i) {
-					int32_t currentEnd = waypoints.size() - 1;
-
-					Waypoint& way = waypoints[i];
-
-					if (i == currentEnd && way.searched == true) {
-						// Abbrechen, da es keinen Weg gibt
-						return false;
-					}
-
-					if (way.searched == true)
-						break;
-
-					way.searched = true;
-					position1 = way.position;
-
-					uint8_t byte = map.getDirections(position1);
-
-					/*
-					Untersuche einzelne Richtungen nach Bewegungsmöglichkeit
-					*/
-					if (byte & Map::NORTH_EAST) {
-						position = position1 - mapWidth + 1;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-					if (byte & Map::SOUTH_EAST) {
-						position = position1 + mapWidth + 1;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-					if (byte & Map::SOUTH_WEST) {
-						position = position1 + mapWidth - 1;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-					if (byte & Map::NORTH_WEST) {
-						position = position1 - mapWidth - 1;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-					if (byte & Map::NORTH) {
-						position = position1 - mapWidth;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-					if (byte & Map::EAST) {
-						position = position1 + 1;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-					if (byte & Map::SOUTH) {
-						position = position1 + mapWidth;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-					if (byte & Map::WEST) {
-						position = position1 - 1;
-
-						if (!searchMap[position]) {
-							waypoints.push_back(Waypoint(i, position));
-
-							searchMap[position] = 1;
-						}
-
-						if (position == position2)
-							goto way_found;
-					}
-
-				}
-			}
-
-			// Weg gefunden
-			way_found:;
-
-			// Letze eingetragene Position holen
-			Waypoint& w = waypoints.back();
-			Waypoint* way = &w;
-
-			while (way->previus != START_WAYPOINT) {
-				path_list.push_back(way->position);
-				way = &waypoints[way->previus];
-			}
-
-			return true;
-		};
+		bool FindWay(uint32_t position1, uint32_t position2, std::list<uint32_t>& path_list);
 };
-
-#undef container
 
 #endif // TSEARCHER_H_INCLUDED
---
+++ cpp/earth2150/src/PathFinder/TSearcher.cpp
@@ -0,0 +1,190 @@
+#include "PathFinder/TSearcher.h"
+
+#include <memory.h>
+
+#define container std::vector
+
+
+TSearcher::TSearcher(const Map& map) :
+	mapSize(map.getWidth() * map.getHeight()),
+	searchMap(new uint8_t[mapSize]),
+	map(map) {};
+
+TSearcher::~TSearcher() {
+	delete searchMap;
+}
+
+/// Hilfsstruktur für abgesuche Wegpunkte
+struct Waypoint {
+	Waypoint(uint32_t previus, uint32_t position) :
+		searched(false), previus(previus), position(position) {}
+
+	bool searched;
+	uint32_t previus;
+	uint32_t position;
+};
+
+bool TSearcher::FindWay(uint32_t position1, uint32_t position2, std::list<uint32_t>& path_list) {
+	// Zu Begin die Suchkarte leeren
+	clearSearchMap();
+
+	// Punktliste
+	container<Waypoint> waypoints;
+
+	// Genügend Speicher Reservieren, damit es nicht zu unnötigen reallocs kommt
+	waypoints.reserve(map.getWidth() * map.getHeight());
+
+	// Startpunkt in Suchliste eintragen
+	waypoints.push_back(Waypoint(START_WAYPOINT, position1));
+
+	uint32_t position = 0;
+
+	const uint16_t mapWidth = map.getWidth();
+
+	while (true) {
+		for (int32_t i = waypoints.size() - 1; i >= 0; --i) {
+			int32_t currentEnd = waypoints.size() - 1;
+
+			Waypoint& way = waypoints[i];
+
+			if (i == currentEnd && way.searched == true) {
+				// Abbrechen, da es keinen Weg gibt
+				return false;
+			}
+
+			if (way.searched == true)
+				break;
+
+			way.searched = true;
+			position1 = way.position;
+
+			uint8_t byte = map.getDirections(position1);
+
+			/*
+			Untersuche einzelne Richtungen nach Bewegungsmöglichkeit
+			*/
+			if (byte & Map::NORTH_EAST) {
+				position = position1 - mapWidth + 1;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+			if (byte & Map::SOUTH_EAST) {
+				position = position1 + mapWidth + 1;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+			if (byte & Map::SOUTH_WEST) {
+				position = position1 + mapWidth - 1;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+			if (byte & Map::NORTH_WEST) {
+				position = position1 - mapWidth - 1;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+			if (byte & Map::NORTH) {
+				position = position1 - mapWidth;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+			if (byte & Map::EAST) {
+				position = position1 + 1;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+			if (byte & Map::SOUTH) {
+				position = position1 + mapWidth;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+			if (byte & Map::WEST) {
+				position = position1 - 1;
+
+				if (!searchMap[position]) {
+					waypoints.push_back(Waypoint(i, position));
+
+					searchMap[position] = 1;
+				}
+
+				if (position == position2)
+					goto way_found;
+			}
+
+		}
+	}
+
+	// Weg gefunden
+	way_found:;
+
+	// Letze eingetragene Position holen
+	Waypoint& w = waypoints.back();
+	Waypoint* way = &w;
+
+	while (way->previus != START_WAYPOINT) {
+		path_list.push_back(way->position);
+		way = &waypoints[way->previus];
+	}
+
+	return true;
+}
+
+void TSearcher::clearSearchMap() {
+	memset(searchMap, 0, mapSize);
+}
