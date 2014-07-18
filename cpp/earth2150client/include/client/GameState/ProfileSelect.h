13e200e6702c0c61721c9bb16b121eb2d563e956
Tirus <Tirus@92bb1c38-9b61-c64a-b0c3-bf70b591afdc>
2014-07-18 12:49:04 +0000

    Neue (GameState) Klasse ProfileSelect: - Stellt eine Profilauswahl dar.
    git-svn-id: https://felix1991.mine.nu/svn/Earth2150@216 92bb1c38-9b61-c64a-b0c3-bf70b591afdc

---
+++ cpp/earth2150client/include/client/GameState/ProfileSelect.h
@@ -0,0 +1,55 @@
+#ifndef PROFILESELECT_H_INCLUDED
+#define PROFILESELECT_H_INCLUDED
+
+#include "client/AbstractGameState.h"
+
+namespace irr {
+namespace gui {
+class IGUIElement;
+class IGUIEnvironment;
+}
+}
+
+class ProfileSelect : public AbstractGameState {
+	private:
+		ProfileSelect(const ProfileSelect&);
+		ProfileSelect& operator=(const ProfileSelect&);
+
+		/// Enumerator für die GUI Elemente des Menüs
+		enum {
+			GUI_WINDOW_MAIN,
+			GUI_COMBOBOX_USERSELECT,
+			GUI_BUTTON_NEWPROFILE,
+			GUI_BUTTON_USEPROFILE,
+			GUI_BUTTON_BACK,
+			GUI_COUNT
+		};
+
+		/// Array der GUI Elemente.
+		irr::gui::IGUIElement* guiElements[GUI_COUNT];
+
+		/// GameState in den gewechselt werden soll
+		AbstractGameState* changeToState;
+		bool gameStateChanged;
+
+		/// Erstellt die GUI Elemente
+		void createGUI(irr::gui::IGUIEnvironment * guienv);
+
+		/// Entfernt die GUI Elemente
+		void removeGUI();
+
+		void changeGameState(AbstractGameState* newState);
+
+	public:
+		ProfileSelect(EngineData& engineData);
+
+		/// Main Methode des Zustandes, Rückgabe bestimmt den Folgezustand.
+		virtual AbstractGameState* run();
+
+		/// Irrlicht Events von GUI, Maus, Tastatur ect...
+		bool OnEvent(const irr::SEvent& event);
+};
+
+
+
+#endif // PROFILESELECT_H_INCLUDED
---
+++ cpp/earth2150client/src/client/GameState/ProfileSelect.cpp
@@ -0,0 +1,87 @@
+#include "client/GameState/ProfileSelect.h"
+
+#include "client/AbstractGameState.h"
+#include "client/GameState/MainMenu.h"
+
+#include "client/EngineData.h"
+#include <irrlicht.h>
+
+using namespace irr;
+
+using namespace video;
+using namespace gui;
+using namespace core;
+
+ProfileSelect::ProfileSelect(EngineData& engineData) :
+	AbstractGameState(engineData),
+	changeToState(0),
+	gameStateChanged(false) {
+}
+
+AbstractGameState* ProfileSelect::run() {
+	IrrlichtDevice* device = engineData.getIrrlichtDevice();
+	IVideoDriver* driver = device->getVideoDriver();
+	IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+	createGUI(guienv);
+
+	while (device->run() && !gameStateChanged) {
+		driver->beginScene(true, true);
+
+		guienv->drawAll();
+
+		driver->endScene();
+	}
+
+	removeGUI();
+
+	return changeToState;
+}
+
+void ProfileSelect::createGUI(irr::gui::IGUIEnvironment * guienv) {
+	IGUIWindow* window = guienv->addWindow(recti(50, 50, 450, 650));
+	guiElements[GUI_WINDOW_MAIN] = window;
+
+	window->setDrawTitlebar(false);
+	window->setDraggable(false);
+	window->getCloseButton()->setVisible(false);
+
+	guienv->addStaticText(L"Verfügbare Profile:", core::recti(10, 5, 390, 25), false, true, window, -1, true);
+
+	guiElements[GUI_COMBOBOX_USERSELECT] = guienv->addListBox(recti(10, 35, 390, 300), window, -1, true);
+
+	guiElements[GUI_BUTTON_NEWPROFILE] = guienv->addButton(recti(20, 320, 380, 360), window, -1, L"Neues Profil erstellen");
+	guiElements[GUI_BUTTON_USEPROFILE] = guienv->addButton(recti(20, 380, 380, 420), window, -1, L"Ausgewähltes Profil verwenden");
+	guiElements[GUI_BUTTON_BACK] = guienv->addButton(recti(20, 440, 380, 480), window, -1, L"Zurück zum Hauptmenü");
+
+}
+
+void ProfileSelect::removeGUI() {
+	guiElements[GUI_WINDOW_MAIN]->remove();
+}
+
+void ProfileSelect::changeGameState(AbstractGameState* newState) {
+	changeToState = newState;
+	gameStateChanged = true;
+}
+
+bool ProfileSelect::OnEvent(const irr::SEvent& event) {
+	if (event.EventType != EET_GUI_EVENT)
+		return false;
+
+	if (event.GUIEvent.EventType != gui::EGET_BUTTON_CLICKED)
+		return false;
+
+	IGUIElement* caller = event.GUIEvent.Caller;
+
+	/*if (caller == guiElements[GUI_BUTTON_NEWGME]) {
+		changeGameState(new TestGameState(engineData));
+		return true;
+	}*/
+
+	if (caller == guiElements[GUI_BUTTON_BACK]) {
+		printf("Back\n");
+		changeGameState(new MainMenu(engineData));
+		return true;
+	}
+}
