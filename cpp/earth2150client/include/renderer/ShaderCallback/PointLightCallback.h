8d4817aaebc187be6de951f30f189a2eaac5c3ae
Tirus <Tirus@92bb1c38-9b61-c64a-b0c3-bf70b591afdc>
2015-09-05 20:38:31 +0000

    PointLightCallback: - Ungenutzen Code im Shader Callback entfernt.
    git-svn-id: https://felix1991.mine.nu/svn/Earth2150@257 92bb1c38-9b61-c64a-b0c3-bf70b591afdc

--- cpp/earth2150client/include/renderer/ShaderCallback/PointLightCallback.h
+++ cpp/earth2150client/include/renderer/ShaderCallback/PointLightCallback.h
@@ -19,34 +19,6 @@ public:
 	{
 		video::IVideoDriver* driver = services->getVideoDriver();
 
-		// set clip matrix
-
-		irr::core::matrix4 worldViewProj;
-		worldViewProj = driver->getTransform(video::ETS_PROJECTION);
-		worldViewProj *= driver->getTransform(video::ETS_VIEW);
-		worldViewProj *= driver->getTransform(video::ETS_WORLD);
-
-
-		//services->setVertexShaderConstant("mWorldViewProj", worldViewProj.pointer(), 16);
-
-
-		/// World View
-		core::matrix4 worldView = driver->getTransform(video::ETS_WORLD) * driver->getTransform(video::ETS_VIEW);
-
-		services->setVertexShaderConstant("mWorldView", worldView.pointer(), 16);
-
-
-		// set camera position
-
-		// set transposed world matrix
-
-		core::matrix4 world = driver->getTransform(video::ETS_WORLD);
-		world = world.getTransposed();
-
-
-		services->setVertexShaderConstant("mTransWorld", world.pointer(), 16);
-
-		// set texture, for textures you can use both an int and a float setPixelShaderConstant interfaces (You need it only for an OpenGL driver).
 		s32 TextureLayerID1 = 0;
 		s32 TextureLayerID2 = 1;
 		s32 TextureLayerID3 = 2;
@@ -55,13 +27,10 @@ public:
 		services->setPixelShaderConstant("NormalTex", &TextureLayerID2, 1);
 		services->setPixelShaderConstant("PositionTex", &TextureLayerID3, 1);
 
-		//services->setPixelShaderConstant("LightColor",
-		//		reinterpret_cast<f32*>(&(pointLight->color)), 4);
 
 		video::SColorf color(currentPointLight.color);
 
 		services->setPixelShaderConstant("LightColor", reinterpret_cast<f32*>(&color), 3);
-
 		services->setPixelShaderConstant("LightPosition", reinterpret_cast<f32*>(&(currentPointLight.position)), 3);
 
 		services->setPixelShaderConstant("strength", &(currentPointLight.radius), 1);
